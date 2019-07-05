34f048b4411cd25de5cb12e00188967644676594
Unknown <arkadochnikova@gmail.com>
2019-07-05 11:33:05 +0100

    Data extraction update
    Read *.txt files
    Plots

--- load_data.m
+++ load_data.m
@@ -1,10 +1,11 @@
 my_init;
 %% Load files
 addpath('foam_data');
-firstLine = 6;
-num_columns = 3;
+firstLine   = 6;
+numColumns  = 3;
+numFiles    = 10;
 
-dataset = 'C'; %'D';
+dataset = 'D'; % 'C'; %
 
 % switch dataset
 %     case 'C'
@@ -13,9 +14,40 @@ dataset = 'C'; %'D';
 %          % paste procedure from below
 % end
 
-for iFile=1:10
-    fileName = ['FOAM',num2str(iFile),dataset,'.txt']; % 
+for iFile=1:numFiles
+    fileName = ['FOAM',num2str(iFile),dataset,'.txt']; % read all as strings
     D = textread(fileName, '%s','delimiter','\n'); % read full file as strings
     V = cellfun(@(s) sscanf(s,'%f').',D, 'un', 0); % read each string in D as numbers
     data_foam{iFile} = vertcat(V{:}); % concentrate arrays
 end
+
+%% Plots
+figure;
+for iFile=1:5
+    plot(data_foam{iFile}(:,1),data_foam{iFile}(:,2)); hold on;
+    names(iFile) = {[dataset,num2str(iFile)]};
+end
+title('Loads');
+legend(names(1:5));
+
+figure;
+for iFile=1:5
+    plot(data_foam{iFile}(:,1),data_foam{iFile}(:,3)); hold on;
+end
+title('Disp')
+legend(names(1:5));
+
+figure;
+for iFile=6:10
+    plot(data_foam{iFile}(:,1),data_foam{iFile}(:,2)); hold on;
+    names(iFile) = {[dataset,num2str(iFile)]};
+end
+title('Loads')
+legend(names(6:10));
+
+figure;
+for iFile=6:10
+    plot(data_foam{iFile}(:,1),data_foam{iFile}(:,3)); hold on;
+end
+title('Disp')
+legend(names(6:10));

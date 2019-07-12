bd9354fab4df6e2406d2feb01f0a2afc0b8f1edf
Anastasia <arkadochnikova@gmail.com>
2019-07-12 10:58:51 +0100

    Creating batches of data
    updated narx
    squared correlation coefficient

--- test_narx.m
+++ test_narx.m
@@ -8,6 +8,20 @@ u = [1:10];
 y = [1:10];
 n_u = 3;
 n_y = 2;
-%%
-narx(u,y,n_u,n_y,theta)
-
+%% Create batch of regressors
+d = n_u + n_y;                                                              % size of input vector x
+lambda = 4;                                                                 % order of polynomial
+times = [1];                                                                % batch over times
+for t = times
+    x(:,t) = [u(t-n_u:t);y(t-n_y:t)];                                       % create input vector 
+    % Computing all polynomial terms
+    iTerm = 0;
+    for iLambda = 1:lambda
+        for k = 1:size(indeces{iLambda},1)
+           iTerm = iTerm + 1;  
+           term(1,iTerm) = regressor(x(:,t),indeces{iLambda}(k,:));         % compute the regressor
+           symb_term{iTerm} = a(indeces{iLambda}(k,:));                     % dictionary of regressors
+        end
+    end
+    nTerms = iTerms;                                                        % total number of regressors
+end
